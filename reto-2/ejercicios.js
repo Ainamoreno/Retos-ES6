3adb514bfcf329317979b253bb3a7d18188bda1d
Juan Genovard <juangenovardcaballero@gmail.com>
2022-10-07 13:35:24 +0200

    error

---
+++ reto-2/ejercicios.js
@@ -0,0 +1,24 @@
+
+
+
+
+class password {
+
+    constructor(){
+
+        this.longitud = 8;
+
+    }
+
+    // metodo
+
+    generarContraseña(longitud){
+
+        const resultado = Math.random().toString(36).substring(0,8);
+        console.log(resultado);
+
+        
+    }
+}
+
+
