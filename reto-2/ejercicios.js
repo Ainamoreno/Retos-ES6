c822fcd9c1581a2d0199a6e3d340fdfdbaafa684
Aina <ainamoreno@outlook.com>
2022-10-07 13:38:14 +0200

    quito espacios

--- reto-2/ejercicios.js
+++ reto-2/ejercicios.js
@@ -1,23 +1,14 @@
-
-
-
-
 class password {
 
-    constructor(){
+    constructor() {
 
         this.longitud = 8;
-
     }
-
+    
     // metodo
-
-    generarContraseña(longitud){
-
+    generarContraseña(longitud) {
         const resultado = Math.random().toString(36).substring(0,8);
         console.log(resultado);
-
-        
     }
 }
 
