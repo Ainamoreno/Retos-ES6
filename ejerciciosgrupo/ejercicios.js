73513c54d39a05e73cf07f61b14752291af88ccc
Juan Genovard <juangenovardcaballero@gmail.com>
2022-10-07 13:16:55 +0200

    reto1

---
+++ ejerciciosgrupo/ejercicios.js
@@ -0,0 +1,28 @@
+
+
+
+class Persona {
+
+    constructor(dni){
+
+        this.nombre = "Juan Macarra";
+        this.edad = 32;
+        this.sexo = "H";
+        this.peso = 65;
+        this.altura = 170;
+        this.dni = dni;
+
+    }
+
+
+}
+
+
+let persona1 = new Persona("20196547x");
+
+let persona2 = new Persona("78996547x");
+
+let persona3 = new Persona("56896211x");
+
+
+
